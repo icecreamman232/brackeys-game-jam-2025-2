c5c9b5bd654473599f5b2fd03194e1b3edfaa317
Stephen Ho <stephen.ho.232@gmail.com>
2025-08-28 02:10:01 +0700

    Adding visual for button

--- Assets/Scripts/UI/ButtonController.cs
+++ Assets/Scripts/UI/ButtonController.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
@@ -6,6 +7,11 @@ namespace SGGames.Scripts.UI
 {
     public class ButtonController : Selectable, IPointerClickHandler
     {
+        [SerializeField] private Sprite m_normalSprite;
+        [SerializeField] private Sprite m_onClickSprite;
+        [SerializeField] private Color m_normalColor;
+        [SerializeField] private Color m_hoverColor;
+        
         public Action OnClickAction;
         
         public void OnPointerClick(PointerEventData eventData)
@@ -13,6 +19,30 @@ namespace SGGames.Scripts.UI
             OnClickButton();
         }
 
+        public override void OnPointerDown(PointerEventData eventData)
+        {
+            ((Image)targetGraphic).sprite = m_onClickSprite;
+            base.OnPointerDown(eventData);
+        }
+
+        public override void OnPointerUp(PointerEventData eventData)
+        {
+            ((Image)targetGraphic).sprite = m_normalSprite;
+            base.OnPointerUp(eventData);
+        }
+
+        public override void OnPointerEnter(PointerEventData eventData)
+        {
+            targetGraphic.color = m_hoverColor;
+            base.OnPointerEnter(eventData);
+        }
+
+        public override void OnPointerExit(PointerEventData eventData)
+        {
+            targetGraphic.color = m_normalColor;
+            base.OnPointerExit(eventData);
+        }
+        
         protected virtual void OnClickButton()
         {
             OnClickAction?.Invoke();
