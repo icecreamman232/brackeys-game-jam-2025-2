a4d90e91f7991a77538f692fc26db2fecffb79ef
Stephen Ho <stephen.ho.232@gmail.com>
2025-08-30 22:32:27 +0700

    Adding version to main menu

--- Assets/Scripts/UI/MainMenuCanvas.cs
+++ Assets/Scripts/UI/MainMenuCanvas.cs
@@ -1,14 +1,17 @@
 using SGGames.Scripts.UI;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuCanvas : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI m_verstionText;
     [SerializeField] private ButtonController m_playButton;
 
     private void Awake()
     {
         m_playButton.OnClickAction = LoadGameplay;
+        m_verstionText.text = $"v{Application.version}";   
     }
 
     private void LoadGameplay()
